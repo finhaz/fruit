496b640c61af90308ea511a4ee4e2ed602cfeac8
finhaz <finhaz@foxmail.com>
2022-05-24 11:36:20 +0800

    准备剥离串口

---
+++ ocean/Communication/CommonRes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace ocean
+{
+    public static class CommonRes
+    {
+        public static SerialPort serialPort1 = new SerialPort();
+    }
+
+}
