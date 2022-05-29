2a36e5458e92defda9b4888eb651af139992f4f5
finhaz <finhaz@foxmail.com>
2022-05-29 21:48:44 +0800

    引入一列修改按钮，实现了同步更新数据库。不过按键多了点

--- ocean/UI/DataAnal.xaml.cs
+++ ocean/UI/DataAnal.xaml.cs
@@ -26,6 +26,8 @@ namespace ocean.UI
         public DataTable dtset { get; set; }
         public DataTable dtfactor { get; set; }
         int select_index;
+        DataGridTemplateColumn col1 = new DataGridTemplateColumn();
+
         public DataAnal()
         {
             InitializeComponent();
@@ -35,7 +37,70 @@ namespace ocean.UI
 
         private void btRUN_Click(object sender, RoutedEventArgs e)
         {
+            //Thread th = new Thread(new ThreadStart(test)); //创建线程
+            //th.Start(); //启动线程
 
+            //PSO_v.pso_init = false;
+            //IPSO_v.pso_init = false;
+            int send_num = 0;
+
+            if (!CommonRes.mySerialPort.IsOpen)
+            {
+                MessageBox.Show("请打开串口！");
+                return;
+            }
+            //Num_time = 0;
+            /*
+            brun = !brun;
+            if (brun)
+            {
+                btRUN.Text = "系统正在运行";
+                btRUN.Content = "停止";
+            }
+            else
+            {
+                textBox1.Text = "系统停止运行";
+                button4.Text = "运行";
+                timer3.Enabled = false;
+            }
+
+            if (Protocol_num == 0)//FE协议
+            {
+                NYS_com.Monitor_Run(brun);
+                send_num = NYS_com.sendbf[4] + 5;
+                serialPort1.Write(NYS_com.sendbf, 0, send_num);
+            }
+            else if (Protocol_num == 1)//modbus
+            {
+                //1号机1通道
+                FCOM2.Monitor_Run(1, 128, brun);
+                send_num = FCOM2.sendbf[0] + 1;
+                serialPort1.Write(FCOM2.sendbf, 0, send_num);
+            }
+
+            for (int i = 0; i < send_num; i++)
+            {
+                try
+                {
+                    //if (Protocol_num == 0)
+                    {
+                       // model.Name += Convert.ToString(NYS_com.sendbf[i], 16);
+                    }
+                    //else if (Protocol_num == 1)
+                    {
+                      //  model.Name += Convert.ToString(FCOM2.sendbf[i], 16);
+                    }
+                    //model.Name += ' ';
+                }
+                catch
+                {
+                    MessageBox.Show("停机冲突");
+                }
+            }
+
+            //model.Name += '\r';
+            //model.Name += '\n';
+            */
         }
 
         private void btSTOP_Click(object sender, RoutedEventArgs e)
@@ -59,20 +124,116 @@ namespace ocean.UI
             dtset = CommonRes.dt2;
 
             dtfactor = CommonRes.dt3;
+
+            col1.Header = "操作";
         }
 
-
-        private void datashow_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
-        {
-            string newValue = (e.EditingElement as TextBox).Text;
-            DB_Access.UpdateDBTable(CommonRes.dt1, "PARAMETER_RUN");
-        }
 
         private void datashow_BeginningEdit(object sender, DataGridBeginningEditEventArgs e)
         {
             select_index = datashow.SelectedIndex;
             string oldValue = datashow.SelectedCells.ToString();
-            
+
         }
+
+
+        private void MButton_Click(object sender, RoutedEventArgs e)
+        {
+            DB_Access.UpdateDBTable(dtrun, "PARAMETER_RUN");
+            datashow.Columns.Remove(col1);
+        }
+
+
+
+
+        private void datashow_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            string newValue = (e.EditingElement as TextBox).Text;
+            
+
+            
+            
+            FrameworkElementFactory factory1 = new FrameworkElementFactory(typeof(Button));
+            //b1.Mode = BindingMode.TwoWay;
+            Binding b1 = new Binding("count");
+            //b1.Mode = BindingMode.
+
+            factory1.SetValue(Button.ContentProperty, "修改");
+            // factory1.SetValue(Button.DataContextProperty,b1);
+            factory1.AddHandler(Button.ClickEvent, new RoutedEventHandler(MButton_Click));
+            DataTemplate cellTemplate1 = new DataTemplate();
+            cellTemplate1.VisualTree = factory1;
+            col1.CellTemplate = cellTemplate1;
+            datashow.Columns.Add(col1);
+
+        }
+
+
+        private void dataset_BeginningEdit(object sender, DataGridBeginningEditEventArgs e)
+        {
+            select_index = datashow.SelectedIndex;
+            datashow.Columns.Remove(col1);
+        }
+
+
+        private void MButton2_Click(object sender, RoutedEventArgs e)
+        {
+            DB_Access.UpdateDBTable(dtset, "PARAMETER_SET");
+            dataset.Columns.Remove(col1);
+        }
+
+        private void dataset_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            string newValue = (e.EditingElement as TextBox).Text;
+
+
+            FrameworkElementFactory factory1 = new FrameworkElementFactory(typeof(Button));
+            //b1.Mode = BindingMode.TwoWay;
+            Binding b1 = new Binding("count");
+            //b1.Mode = BindingMode.
+
+            factory1.SetValue(Button.ContentProperty, "修改");
+            // factory1.SetValue(Button.DataContextProperty,b1);
+            factory1.AddHandler(Button.ClickEvent, new RoutedEventHandler(MButton2_Click));
+            DataTemplate cellTemplate1 = new DataTemplate();
+            cellTemplate1.VisualTree = factory1;
+            col1.CellTemplate = cellTemplate1;
+            dataset.Columns.Add(col1);
+        }
+
+
+        private void datafactor_BeginningEdit(object sender, DataGridBeginningEditEventArgs e)
+        {
+            select_index = datashow.SelectedIndex;
+
+        }
+
+
+        private void MButton3_Click(object sender, RoutedEventArgs e)
+        {
+            DB_Access.UpdateDBTable(dtfactor, "PARAMETER_FACTOR");
+            datafactor.Columns.Remove(col1);
+        }
+
+        private void datafactor_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            string newValue = (e.EditingElement as TextBox).Text;
+           
+            FrameworkElementFactory factory1 = new FrameworkElementFactory(typeof(Button));
+            //b1.Mode = BindingMode.TwoWay;
+            Binding b1 = new Binding("count");
+            //b1.Mode = BindingMode.
+
+            factory1.SetValue(Button.ContentProperty, "修改");
+            // factory1.SetValue(Button.DataContextProperty,b1);
+            factory1.AddHandler(Button.ClickEvent, new RoutedEventHandler(MButton3_Click));
+            DataTemplate cellTemplate1 = new DataTemplate();
+            cellTemplate1.VisualTree = factory1;
+            col1.CellTemplate = cellTemplate1;
+            datafactor.Columns.Add(col1);
+
+        }
+
+
     }
 }
