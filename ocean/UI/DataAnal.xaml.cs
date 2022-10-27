cf5a8c5c802561c4a35a2bb063503861e930cb14
finhaz <finhaz@foxmail.com>
2022-10-27 17:14:48 +0800

    加入了表中每行数据的按钮下发数据功能，目前不能选择协议，默认还是FE协议

--- ocean/UI/DataAnal.xaml.cs
+++ ocean/UI/DataAnal.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Diagnostics;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -34,6 +35,32 @@ namespace ocean.UI
         DataBase_Interface DB_Com = new DataBase_Interface();
         string newValue;
 
+        bool brun = false;
+        //bool bshow = false;
+        //bool bmodify = false;
+        //bool Initialized = true;//调试参数与修正系数核验标志位
+        //bool flag_uncon = false;//上下参数不一致标志位
+        //bool flag_get_runvalue = false;//读取运行数据标志位
+
+
+        public bool flag_under_first = false;
+        public bool flag_upper_first = false;
+
+        bool sopen = false;
+        float old_value;
+        float new_value;
+        public int sn = 0;
+        int mrow;
+        int Num_time = 0;
+        int Num_DSP = 0;
+        int Protocol_num = 0;
+        static int Set_Num_DSP = 2;//代表有机子数
+
+
+        //通讯协议
+        Message NYS_com = new Message();
+        Message_modbus FCOM2 = new Message_modbus();
+
         public DataAnal()
         {
             InitializeComponent();
@@ -42,6 +69,7 @@ namespace ocean.UI
 
         private void btRUN_Click(object sender, RoutedEventArgs e)
         {
+            //bool brun;
             //Thread th = new Thread(new ThreadStart(test)); //创建线程
             //th.Start(); //启动线程
 
@@ -55,32 +83,23 @@ namespace ocean.UI
                 return;
             }
             //Num_time = 0;
-            /*
-            brun = !brun;
-            if (brun)
-            {
-                btRUN.Text = "系统正在运行";
-                btRUN.Content = "停止";
-            }
-            else
-            {
-                textBox1.Text = "系统停止运行";
-                button4.Text = "运行";
-                timer3.Enabled = false;
-            }
+
+            brun = true;
+
+            textBox1.Text= "系统正在运行";
 
             if (Protocol_num == 0)//FE协议
             {
                 NYS_com.Monitor_Run(brun);
                 send_num = NYS_com.sendbf[4] + 5;
-                serialPort1.Write(NYS_com.sendbf, 0, send_num);
+                CommonRes.mySerialPort.Write(NYS_com.sendbf, 0, send_num);
             }
             else if (Protocol_num == 1)//modbus
             {
                 //1号机1通道
                 FCOM2.Monitor_Run(1, 128, brun);
                 send_num = FCOM2.sendbf[0] + 1;
-                serialPort1.Write(FCOM2.sendbf, 0, send_num);
+                CommonRes.mySerialPort.Write(FCOM2.sendbf, 0, send_num);
             }
 
             for (int i = 0; i < send_num; i++)
@@ -102,15 +121,30 @@ namespace ocean.UI
                     MessageBox.Show("停机冲突");
                 }
             }
-
             //model.Name += '\r';
             //model.Name += '\n';
-            */
+
         }
 
         private void btSTOP_Click(object sender, RoutedEventArgs e)
         {
-
+            //bool brun;
+            int send_num = 0;
+            brun =false;
+            textBox1.Text = "系统停止运行";
+            if (Protocol_num == 0)//FE协议
+            {
+                NYS_com.Monitor_Run(brun);
+                send_num = NYS_com.sendbf[4] + 5;
+                CommonRes.mySerialPort.Write(NYS_com.sendbf, 0, send_num);
+            }
+            else if (Protocol_num == 1)//modbus
+            {
+                //1号机1通道
+                FCOM2.Monitor_Run(1, 128, brun);
+                send_num = FCOM2.sendbf[0] + 1;
+                CommonRes.mySerialPort.Write(FCOM2.sendbf, 0, send_num);
+            }
         }
 
         private void Page_Loaded(object sender, EventArgs e)
@@ -159,9 +193,6 @@ namespace ocean.UI
         {
             //读取选中行
             var x = dataset.SelectedIndex;
-            string y = dtset.Rows[0][0].ToString();
-            int z=Convert.ToInt32(y);
-            select_index  = x+z;
         }
 
 
@@ -171,11 +202,27 @@ namespace ocean.UI
             var x = dataset.SelectedIndex;
             string y = dtset.Rows[0][0].ToString();
             int z = Convert.ToInt32(y);
-            select_index = x + z;
+            int tempsn = x + z;
             string val = dtset.Rows[x][5].ToString();
             float value=Convert.ToSingle(val);
             DB_Com.DataBase_SET_Save("PARAMETER_SET", value, (byte)select_index);
-
+            if (CommonRes.mySerialPort.IsOpen==true)
+            {
+                if (Protocol_num == 0)
+                {
+                    NYS_com.Monitor_Set((byte)tempsn, (byte)(DB_Com.data[tempsn].COMMAND), value);
+                    CommonRes.mySerialPort.Write(NYS_com.sendbf, 0, NYS_com.sendbf[4] + 5);
+                }
+                else if (Protocol_num == 1)
+                {
+                    FCOM2.Monitor_Set_06(tempsn, value);
+                    CommonRes.mySerialPort.Write(FCOM2.sendbf, 0, 8);
+                }
+            }
+            else
+            {
+                MessageBox.Show("请打开串口！");
+            }
         }
 
 
@@ -187,13 +234,38 @@ namespace ocean.UI
 
         private void datafactor_BeginningEdit(object sender, DataGridBeginningEditEventArgs e)
         {
-            select_index = datashow.SelectedIndex;
+            
+            var x = datafactor.SelectedIndex;
         }
 
 
         private void MButton3_Click(object sender, RoutedEventArgs e)
         {
-            DB_Access.UpdateDBTable(dtfactor, "PARAMETER_FACTOR");
+            //读取选中行
+            var x = datafactor.SelectedIndex;           
+            string y = dtfactor.Rows[0][0].ToString();
+            int z = Convert.ToInt32(y);
+            int tempsn = x + z;
+            string val = dtfactor.Rows[x][2].ToString();
+            float value = Convert.ToSingle(val);
+            DB_Com.DataBase_SET_Save("PARAMETER_FACTOR", value, (byte)select_index);
+            if (CommonRes.mySerialPort.IsOpen==true)
+            {
+                if (Protocol_num == 0)
+                {
+                    NYS_com.Monitor_Set((byte)tempsn, (byte)(DB_Com.data[tempsn].COMMAND), value);
+                    CommonRes.mySerialPort.Write(NYS_com.sendbf, 0, NYS_com.sendbf[4] + 5);
+                }
+                else if (Protocol_num == 1)
+                {
+                    FCOM2.Monitor_Set_06(tempsn, value);
+                    CommonRes.mySerialPort.Write(FCOM2.sendbf, 0, 8);
+                }
+            }
+            else
+            {
+                MessageBox.Show("请打开串口！");
+            }
         }
 
         private void datafactor_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
